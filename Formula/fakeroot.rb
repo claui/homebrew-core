e2ee35637a656e283628ca8116e580ef1d523351
Claudia <claui@users.noreply.github.com>
2020-11-16 00:43:06 +0100

    fakeroot 1.25.3

--- Formula/fakeroot.rb
+++ Formula/fakeroot.rb
@@ -1,8 +1,8 @@
 class Fakeroot < Formula
   desc "Provide a fake root environment"
   homepage "https://tracker.debian.org/pkg/fakeroot"
-  url "https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.24.orig.tar.gz"
-  sha256 "2e045b3160370b8ab4d44d1f8d267e5d1d555f1bb522d650e7167b09477266ed"
+  url "https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.25.3.orig.tar.gz"
+  sha256 "8e903683357f7f5bcc31b879fd743391ad47691d4be33d24a76be3b6c21e956c"
   license "GPL-3.0"
 
   livecheck do
